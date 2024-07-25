10e827ac2e2e7010920e50b350f594271e9e8d2c
FloWa <fwalzel@gmx.de>
2024-07-25 08:36:49 +0200

    refactor/test: marked functions __flattenObj, __mapArrayToObj, __getValueFromPath as helper functions. Preopared writing tests for i18n-deepl.js

--- src/i18n-deepl.js
+++ src/i18n-deepl.js
@@ -30,6 +30,82 @@
 import deepl from 'deepl-node';
 import fs from 'async-file-tried';
 
+
+/****************************************
+ * HELPER FUNCTIONS
+ ****************************************/
+
+/**
+ * Flatten an object by recursively writing its values to an array
+ *
+ * @param obj
+ * @returns {*[]}
+ */
+function __flattenObj(obj) {
+  const result = [];
+
+  function recurse(curr) {
+    for (let key in curr) {
+      if (typeof curr[key] === 'object' && curr[key] !== null) {
+        recurse(curr[key]);
+      } else {
+        result.push(curr[key]);
+      }
+    }
+  }
+
+  recurse(obj);
+  return result;
+}
+
+/**
+ * The inverse operation of __flattenObject(): maps the values of a flat array back to a given object
+ *
+ * @param obj
+ * @param values
+ * @returns {*}
+ */
+function __mapArrayToObj(obj, values, childParam) {
+  let index = 0;
+
+  function recurse(curr) {
+    for (let key in curr) {
+      if (typeof curr[key] === 'object' && curr[key] !== null) {
+        recurse(curr[key]);
+      } else {
+        curr[key] = values[index++][childParam];
+      }
+    }
+  }
+
+  recurse(obj);
+  return obj;
+}
+
+/** Traverse an object by a given path of sub-nodes and retrieve its value
+ *
+ * @param obj
+ * @param path | given like "my.path.to", will retrieve {my: {path: {to: "Value" }}}
+ * @returns {*}
+ */
+function __getValueFromPath(obj, path) {
+  // Split the path into an array of keys
+  const keys = path.split('.');
+
+  // Use reduce to traverse the object
+  return keys.reduce((acc, key) => {
+    if (acc && acc.hasOwnProperty(key)) {
+      return acc[key];
+    }
+    return undefined; // Return undefined if any key is not found
+  }, obj);
+}
+
+
+/****************************************
+ * PUBLIC FUNCTIONS
+ ****************************************/
+
 /**
  * Write th DeepL auth key to .env file
  *
@@ -41,7 +117,7 @@ async function setAuthKey(key) {
     throw new Error('Provided argument is not a valid deepl auth key.')
   }
   const file = 'deepl-auth.env';
-  let [res, err] = await fs.writeFile(file, `export DEEPL_AUTH_KEY='${key}'`, 'utf8');
+  let [res, err] = await fs.writeFile(file, `export DEEPL_AUTH_KEY='${key}'`);
   if (err) {
     console.error(`Unable to read file ${file}`);
     throw err;
@@ -72,7 +148,7 @@ async function getSupportedLanguages(authKey) {
 
 
 /**
- * Function to translate a string or an array of strings using the DeepL API
+ * Translate a string or an array of strings using the DeepL API
  *
  * @param authKey
  * @param texts
@@ -91,71 +167,6 @@ async function translateTexts(authKey, texts, sourceLang, targetLang, options) {
   return res;
 }
 
-/**
- * Flatten an object by recursively writing the values to an array
- *
- * @param obj
- * @returns {*[]}
- */
-function flattenObject(obj) {
-  const result = [];
-
-  function recurse(curr) {
-    for (let key in curr) {
-      if (typeof curr[key] === 'object' && curr[key] !== null) {
-        recurse(curr[key]);
-      } else {
-        result.push(curr[key]);
-      }
-    }
-  }
-
-  recurse(obj);
-  return result;
-}
-
-/**
- * The inverse operation of flattenObject(): maps the values of a flat array back to a given object
- *
- * @param obj
- * @param values
- * @returns {*}
- */
-function mapArrayToObject(obj, values, childParam) {
-  let index = 0;
-
-  function recurse(curr) {
-    for (let key in curr) {
-      if (typeof curr[key] === 'object' && curr[key] !== null) {
-        recurse(curr[key]);
-      } else {
-        curr[key] = values[index++][childParam];
-      }
-    }
-  }
-
-  recurse(obj);
-  return obj;
-}
-
-/**
- *
- * @param obj
- * @param path
- * @returns {*}
- */
-function getValueFromPath(obj, path) {
-  // Split the path into an array of keys
-  const keys = path.split('.');
-
-  // Use reduce to traverse the object
-  return keys.reduce((acc, key) => {
-    if (acc && acc.hasOwnProperty(key)) {
-      return acc[key];
-    }
-    return undefined; // Return undefined if any key is not found
-  }, obj);
-}
 
 /**
  *
@@ -172,7 +183,7 @@ async function readI18nJson(file, subNode) {
   //console.log(`result reading JSON:`);
   //console.log(res);
   if (typeof subNode === 'string') {
-    const subEntry = getValueFromPath(res, subNode);
+    const subEntry = __getValueFromPath(res, subNode);
     if (subEntry)
       return subEntry;
     else
@@ -200,9 +211,9 @@ async function translateJSON(authKey, JsonSrc, JsonTarget, targetLang, sourceLan
   console.log(`targetLang: ${targetLang}`);
 
   const srcObj = await readI18nJson(JsonSrc, sourceLang);
-  const translValues = flattenObject(srcObj);
+  const translValues = __flattenObj(srcObj);
   const translRes = await translateTexts(authKey, translValues, sourceLang, targetLang, options);
-  const translObj = mapArrayToObject(srcObj, translRes, 'text');
+  const translObj = __mapArrayToObj(srcObj, translRes, 'text');
   //todo: if JSON target = null append to existing
   const [res, err] = await fs.writeJson(JsonTarget, translObj);
   if (err) {
